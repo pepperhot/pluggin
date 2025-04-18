82b8b53e06d712fd18504d7cb0d9439ffe894dea
pepperhot <lucas.gemo77@gmail.com>
2025-04-18 14:17:01 +0200

    ajout js

--- content.js
+++ content.js
@@ -13,8 +13,8 @@ function updateImagePosition() {
   const rect = player.getBoundingClientRect();
 
   // Centrage vertical parfait
-  const top = window.scrollY + rect.top + rect.height / 2 - img.naturalHeight / 2;
-  const left = window.scrollX + rect.left - img.naturalWidth - 100;
+  const top = window.scrollY + rect.top + rect.height -50 - img.naturalHeight / 2;
+  const left = window.scrollX + rect.left - img.naturalWidth - 1000;
 
   img.style.top = `${top}px`;
   img.style.left = `${left}px`;

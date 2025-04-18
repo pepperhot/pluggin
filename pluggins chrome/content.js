113b3868c7fcf76ed0a5138abbe36555e2d5a4a6
pepperhot <lucas.gemo77@gmail.com>
2025-04-18 13:49:45 +0200

    vfdef

--- pluggins chrome/content.js
+++
@@ -1,4 +0,0 @@
-const img = document.createElement("img");
-img.src = "https://github.com/pepperhot/pluggin/blob/main/pluggins%20chrome/sus.png";
-img.className = "yt-watcher";
-document.body.appendChild(img);
